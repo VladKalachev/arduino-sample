e85b8fcd44403c82735570513f5daaf94d67a826
Corvax <vlvete1990@yandex.ru>
2024-06-13 22:13:26 +0300

    feature: add btn toggle example

---
+++ example2/example2.ino
@@ -0,0 +1,28 @@
+const int LED=9;
+const int BUTTON=2;
+boolean lastButton = LOW;
+boolean currentBytton = LOW;
+boolean ledOn = false;
+
+void setup() {
+  pinMode (LED,OUTPUT);
+  pinMode (BUTTON,INPUT);
+}
+
+boolean debounce(boolean last) {
+  boolean current = digitalRead(BUTTON);
+  if(last != current) {
+    delay(5);
+    current = digitalRead(BUTTON);
+  }
+  return current;
+}
+
+void loop() {
+  currentBytton = debounce(lastButton);
+  if(lastButton == LOW && currentBytton == HIGH) {
+    ledOn = !ledOn;
+  }
+  lastButton = currentBytton;
+  digitalWrite(LED,ledOn);
+}
