bee6d2c48b5fd8bf8085d07627a6555c32cb4a2a
Corvax <vlvete1990@yandex.ru>
2024-06-14 13:47:09 +0300

    feature: add example 5

---
+++ example5/example5.ino
@@ -0,0 +1,15 @@
+int TEMP = 0;
+
+void setup() {
+ Serial.begin(9600);
+}
+
+void loop() {
+  int reading = analogRead(0);
+  float voltage = reading * 5.0;
+    voltage /= 1024.0;
+  Serial.print(voltage); Serial.println(" volts");
+  float temperatureC = voltage * 100;
+  Serial.print (temperatureC); Serial.println(" degrees C");
+  delay(1000);
+}
